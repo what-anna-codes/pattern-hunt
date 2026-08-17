26ff683fd5314203ce965fda022f41d49fdf42f1
what-anna-codes <annakarpiuk@zohomail.eu>
2026-08-17 20:53:47 +0200

    fix: codegen config

--- codegen.ts
+++ codegen.ts
@@ -1,21 +1,15 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 import './envConfig.ts'
- 
+
 const config: CodegenConfig = {
   schema: process.env.NEXT_PUBLIC_API,
   documents: ["src/**/*.tsx", "src/**/*.graphql"],
   generates: {
-    "./src/__generated__/": {
-      preset: "client",
-      presetConfig: {
-        gqlTagName: "gql"
-      },
-    },
     "./src/__generated__/types.ts": {
-      plugins: ["typescript", "typescript-operations"],
+      plugins: ["typescript", "typescript-operations", "typescript-react-apollo"],
     },
-    "./src/__generated/graphql.schema.json": {
-      plugins: [ "introspection"],
+    "./src/__generated__/graphql.schema.json": {
+      plugins: ["introspection"],
       schema: process.env.NEXT_PUBLIC_API
     }
   },
