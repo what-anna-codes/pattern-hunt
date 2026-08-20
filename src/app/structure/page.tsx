f3bc8253fb3cdb74e5e4e5504e50d2636f915e73
what-anna-codes <annakarpiuk@zohomail.eu>
2026-08-20 07:37:58 +0200

    feat: add sample cards grid (test)

--- src/app/structure/page.tsx
+++ src/app/structure/page.tsx
@@ -4,6 +4,10 @@ import "./StructurePage.css";
 import { useWindowSize } from "@uidotdev/usehooks";
 import { GameActions } from "./GameActions/GameActions";
 import { getLayoutType } from "./StructurePageUtils";
+import Card from "@/src/components/Card/Card";
+import { CardStatuses } from "@/src/ts/types";
+import { sampleCardIds } from "@/src/utils/deck";
+import Grid from "@/src/components/Grid/Grid";
 
 export default function StructurePage() {
   return (
@@ -21,7 +25,19 @@ export const StructurePageContent = () => {
     <div className="page-cnt">
       <div className="top-cnt">header</div>
       <div className="center-cnt">
-        <div className="cards-cnt">cards area</div>
+        <div className="cards-cnt">
+          <Grid classNames="z-10" isExpanded={false}>
+            {sampleCardIds?.slice(0, 12).map((id: string, i: number) => {
+              return (
+                <Card
+                  handleClick={() => console.log(id)}
+                  status={CardStatuses.Active}
+                  id={id}
+                />
+              );
+            })}
+          </Grid>
+        </div>
         <div className="sidebar-cnt">
           {mobileLandscape ? <GameActions /> : "sidebar"}
         </div>
